a29924a7fc35c100fd3ce4e2229a3dbca1025406
saptak88 <saptak8837@gmail.com>
2024-08-13 23:35:11 +0530

    express static added

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,11 @@ app.use(cookieParser());
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/chat", chatRoutes);
 
-//app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-//app.get("*", (req, res) => {
-// res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-//});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
